f090e267e11a0b6439dfd0ab4d097d421059ad24
uuurs <zhongrenju2016@gmail.com>
2017-11-19 21:03:20 +0800

    changed mystyle to photo idea

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171119053719) do
+ActiveRecord::Schema.define(version: 20171119094702) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20171119053719) do
     t.integer  "wage_lower_bound"
     t.string   "contact_email"
     t.boolean  "is_hidden",        default: true
+    t.string   "schedule"
   end
 
   create_table "resumes", force: :cascade do |t|
