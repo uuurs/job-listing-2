c5cfc7dcd2692d0461a506f18f21ad7224370b01
uuurs <zhongrenju2016@gmail.com>
2017-11-19 13:44:19 +0800

    add carrierwave for attenchment upload

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171119051040) do
+ActiveRecord::Schema.define(version: 20171119053719) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -27,8 +27,10 @@ ActiveRecord::Schema.define(version: 20171119051040) do
     t.integer  "job_id"
     t.integer  "user_id"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "attatchment"
+    t.string   "attachment"
   end
 
   create_table "users", force: :cascade do |t|
